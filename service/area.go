c196f59931fc75b80cea8b7667c1e5b1d3b2ed9a
liuming <shallmin93203x>
2021-08-23 18:57:14 +0800

    查询体检人列表

--- service/area.go
+++ service/area.go
@@ -8,7 +8,7 @@ import (
 func DistrictList(cityName string) *[]resp.DistrictListRes {
 	sql := "select distinct\n       hb.city_id,\n       hb.city_name,\n       hb.district_id,\n       hb.district_name\nfrom his_branch hb\nwhere city_name like ?"
 	districtListRes := make([]resp.DistrictListRes, 0)
-	err := global.MySqlx.Select(districtListRes, sql, cityName+"%")
+	err := global.MySqlx.Select(&districtListRes, sql, cityName+"%")
 	if err != nil {
 		panic(err.Error())
 	}
